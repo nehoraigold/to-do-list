3d5bf025d33400859b7aaad58a967f1092a72881
Ori Gold <nehoraigold@gmail.com>
2018-12-04 16:15:28 +0200

    Adding items is now possible

--- js/logic.js
+++ js/logic.js
@@ -7,15 +7,13 @@ class ListItem extends React.Component {
         }
     }
 
-
-
     render() {
         return (
             <div className='card'>
                 <div className='card-body'>
                     <label>
-                        <span><input type='checkbox' /></span>
-                        <span>Sample list item</span>
+                        <span><input type='checkbox' className='add-item'/></span>
+                        <span>{this.props.item.task}</span>
                     </label>
                 </div>
             </div>
@@ -24,16 +22,57 @@ class ListItem extends React.Component {
     }
 }
 
+class AddListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.createListItem = this.createListItem.bind(this);
+    }
+
+    createListItem() {
+        var newItem = {
+            task: this.input.value
+        }
+        this.props.handleAdd(newItem);
+        this.input.value = "";
+    }
+
+    render() {
+        return (
+            <div className='card'>
+                <div className='card-body'>
+                    <input type='text' placeholder='Add a new task...' className='add-item' ref={(input) => this.input = input}/>
+                    <button className='btn add-button' onClick={this.createListItem}>Add</button>
+                </div>
+            </div>
+        )
+    }
+}
+
 class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.addItemToList = this.addItemToList.bind(this);
+        this.state = {
+            listItems: []
+        }
+    }
+
+    addItemToList(item) {
+        var allItemsAsString = JSON.stringify(this.state.listItems);
+        var allItems = JSON.parse(allItemsAsString);
+        allItems.push(item);
+        this.setState({
+            listItems: allItems
+        })
+    }
+
     render() {
         return (
             <div className='list'>
                 <h3 className='list-title'>To Do</h3>
+                <AddListItem handleAdd={this.addItemToList} />
                 <div className='list-items'>
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
+                    {this.state.listItems.map(item => <ListItem item={item}/>)}
                 </div>
             </div>
         )
